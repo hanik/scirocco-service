b278395fb8d29e96ffd0196178b73bfdf2313f86
actmember <estyle81@gmail.com>
2018-09-13 08:23:22 +0900

    move server

--- client/src/vuex/actions.js
+++ client/src/vuex/actions.js
@@ -33,17 +33,9 @@ let processResponse = (store, loginResponse) => {
 }
 
 export default {
-    async login (uid, password) {
-        try {
-            const getUserInfoPromise = await getUserInfo(uid, password)
-            const isFinishedPromise = await isFinished(uid)
-            const [userInfoResponse, isFinishedResponse] = await Promise.all([getUserInfoPromise, isFinishedPromise])
-            if (userInfoResponse.data.length === 0) return 'noAuth'
-            if (isFinishedResponse.data[0].CNT > 0) return 'done'
-            axios.defaults.headers.common['Authorization'] = userInfoResponse.jwt // Json Web Token을 request header에 넣는다
-            return userInfoResponse
-        } catch (err) {
-            console.error(err)
-        }
+    async login (store, {uid, password}) {
+        let loginResponse = await api.login(uid, password)
+        processResponse(store, loginResponse)
+        return store.getters.getIsAuth  // 로그인 결과를 리턴한다
     }
 }
