cb7d4e2ac5722974945f34b3e28d30bc11647b46
actmember <estyle81@gmail.com>
2018-09-13 16:25:52 +0900

    reset client

--- README.md
+++
@@ -1,3 +0,0 @@
-# scirocco-service
-
-Scirocco-service
--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
+require('rootpath')();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+const cors = require('cors');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 
-app.use(logger('dev'));
+app.use(cors());
+// app.use(jwt());
+app.use(errorHandler);
+
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', require('./users/users.controller'));
+// app.use('/users', usersRouter);
 
 module.exports = app;
