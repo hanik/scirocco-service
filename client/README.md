c69572d4a18446e20c988c416bb122e4b95c79f9
actmember <estyle81@gmail.com>
2018-09-13 16:32:16 +0900

    reset client

--- client/README.md
+++ client/README.md
@@ -1,4 +1,4 @@
-# scirocco-front
+# client
 
 ## Project setup
 ```
--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue';
-import VeeValidate from 'vee-validate';
 import App from './App.vue';
-import router from './router/';
-import store from './vuex/store';
-import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
-Vue.use(VeeValidate);
 Vue.config.productionTip = false;
 
 new Vue({
@@ -13,5 +10,3 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app');
-
-// https://lovemewithoutall.github.io/it/vue-login-demo/
