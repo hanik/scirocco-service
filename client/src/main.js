64f94168d8c09681cfbd1ab076f63cfd63489e0b
actmember <estyle81@gmail.com>
2018-09-13 19:14:12 +0900

    addd client and server

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import { store } from './store/index';
+import { router } from './router';
 
 Vue.config.productionTip = false;
 
+// setup fake backend
+import { configureFakeBackend } from './helpers';
+configureFakeBackend();
+
 new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app');
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
+});
+
+
+// https://github.com/cornflourblue/vue-vuex-jwt-authentication-example
--- client/src/router.js
+++ client/src/router.js
@@ -1,25 +1,39 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+
+import HomePage from './views/Home'
+import LoginPage from './components/LoginPage'
 
 Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+export const router = new Router({
+    mode: 'history',
+    routes: [
+        {path: '/', component: HomePage},
+        {path: '/login', component: LoginPage},
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+        },
+        // otherwise redirect to home
+        {path: '*', redirect: '/'},
+
+]
 });
+
+router.beforeEach((to, from, next) => {
+    // redirect to login page if not logged in and trying to access a restricted page
+    const publicPages = ['/login'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
+
+    if (authRequired && !loggedIn) {
+        return next('/login');
+    }
+
+    next();
+})
