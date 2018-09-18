d0ef550b0574a1c3683da12caf5e9b49924c0e03
actmember <estyle81@gmail.com>
2018-09-18 15:56:33 +0900

    모르겠어~

--- client/src/router.js
+++ client/src/router.js
@@ -42,15 +42,15 @@ export const router = new Router({
     ]
 });
 
-router.beforeEach((to, from, next) => {
-    // redirect to login page if not logged in and trying to access a restricted page
-    const publicPages = ['/login'];
-    const authRequired = !publicPages.includes(to.path);
-    const loggedIn = localStorage.getItem('user');
-
-    if (authRequired && !loggedIn) {
-        return next('/login');
-    }
-
-    next();
-})
+// router.beforeEach((to, from, next) => {
+//     // redirect to login page if not logged in and trying to access a restricted page
+//     const publicPages = ['/login'];
+//     const authRequired = !publicPages.includes(to.path);
+//     const loggedIn = localStorage.getItem('user');
+//
+//     if (authRequired && !loggedIn) {
+//         return next('/login');
+//     }
+//
+//     next();
+// })
