080b421664a724689c5adb132d293fff1f0ce43f
mabel <tandri6@gmail.com>
2018-09-18 14:01:17 +0900

    Add content and arrow tab with directory

---
+++ client/src/components/step/ContentLearning.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="content-learning">
+    <span>content learning</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "content-learning"
+  }
+</script>
+
+<style scoped lang="scss">
+div {
+  display: none;
+}
+</style>
---
+++ client/src/components/step/ContentRestartService.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="content-restartService">
+    <span>content restart service</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "content-restart-service"
+  }
+</script>
+
+<style scoped lang="scss">
+div {
+  display: none;
+}
+</style>
