658a2aedb482c3723274f8029e58bf2df955815f
Kate <juyeon.kate@gmail.com>
2018-09-19 10:32:31 +0900

    테스트라이브러리 mocha -> jest 로 변경  - 설정파일 추가

---
+++ client/jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    moduleFileExtensions: [
+        'js',
+        'json',
+        'vue'
+    ],
+    // To avoid errors when importing non JavaScript assets.
+    transform: {
+        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.js$': 'babel-jest',
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    // beautifier
+    snapshotSerializers: [
+        'jest-serializer-vue'
+    ],
+    testMatch: [
+        '**/tests/unit/**/*.spec.(js|ts|tsx)|**/__tests__/*.(js|ts|tsx)'
+    ],
+    transformIgnorePatterns: [
+        '/node_modules/.*',
+    ]
+}
