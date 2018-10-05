cab8ba131ee8c0e007dfa216cb11beb50b1d91ab
Kate.M <juyeon.kate@gmail.com>
2018-10-05 20:06:43 +0900

    jest 오류 수정용 스크립트 내용 추가

--- client/tests/unit/jest-clear-cache.sh
+++ client/tests/unit/jest-clear-cache.sh
@@ -1,1 +1,4 @@
-../../node_modules/jest/bin/jest.js --clearCache
+# export VUE_CLI_BABEL_TARGET_NODE=true
+# export VUE_CLI_BABEL_TRANSPILE_MODULES=true
+# ./node_modules/jest/bin/jest.js --clearCache
+# node --inspect-brk ./node_modules/jest/bin/jest.js -i
